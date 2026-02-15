75ed35cf8df71d86c6aeb03ee6db9291fd096806
Gregory Sedykh <62842044+gregorysedykh@users.noreply.github.com>
2026-02-15 17:49:28 +0100

    change e2e testing

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,21 +1,15 @@
 /// <reference types="node" />
 import { defineConfig, devices } from '@playwright/test';
+
 export default defineConfig({
   testDir: './e2e',
-  testIgnore: ['**/fullstack/**'],
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  forbidOnly: Boolean(process.env.CI),
+  retries: process.env.CI ? 1 : 0,
   reporter: [['html', { open: 'never' }], ['list']],
   use: {
     baseURL: 'http://127.0.0.1:4200',
     trace: 'on-first-retry',
-  },
-  webServer: {
-    command: 'npm run start -- --host 127.0.0.1 --port 4200',
-    url: 'http://127.0.0.1:4200',
-    reuseExistingServer: !process.env.CI,
-    timeout: 120_000,
   },
   projects: [
     {
